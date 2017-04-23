fbe9645c7412a85d6ac408407dccf5b1d0f50a6b
DejonGill <dejonjgill@gmail.com>
2017-04-22 21:01:24 -0700

    numpy

--- reader.py
+++ reader.py
@@ -1,1 +1,10 @@
+import numpy as np
+# import matplotlib.pyplot as plt
+import pandas as pd
+import sys
 
+df = pd.read_csv('../test_data/baseballdatabank-2017.1/core/Salaries.csv')
+
+current = df.loc[df['yearID'] >= 2016]
+
+print(current)
